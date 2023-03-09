dc6c3c5a8632b54e81412b24079c2c67ce97f5f0
Sebastianrivas77 <juansebastianrivas67@gmail.com>
2023-03-08 22:56:28 -0300

    Ejercicio 01.js resuelto

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -11,7 +11,7 @@ const nuevoString = "Sebastians";
 const nuevoNumero = 0;
 
 // Crea una variable de tipo boolean.
-const nuevoBoolean = null;
+const nuevoBoolean = true;
 
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - 7 === 3;
