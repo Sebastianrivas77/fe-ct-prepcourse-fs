84dbbfdd0d8ef15cfc1ee01950482c5cd9054ea2
Sebastianrivas77 <juansebastianrivas67@gmail.com>
2023-03-09 00:39:19 -0300

    Ejercicios 03.js

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = "Sebastians";
+const nuevoString = "Sebastian";
 
 // Crea una variable de tipo number.
 const nuevoNumero = 0;
